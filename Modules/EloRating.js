a55ea7a25d77dbc6bf449b22da8a2fb2088113f4
Cem Gencer <cem.gencer@me.com>
2017-11-02 15:59:55 +0300

    elorating

--- Modules/EloRating.js
+++ Modules/EloRating.js
@@ -1,64 +1,67 @@
-EloRating = (function() {
+var EloRating = {};
+EloRating.StartValue = 1200;
 
-    function EloRating(ratingA, ratingB, scoreA, scoreB) {
-      var expectedScores, newRatings;
-      this.KFACTOR = 16;
-      this._ratingA = ratingA;
-      this._ratingB = ratingB;
-      this._scoreA = scoreA;
-      this._scoreB = scoreB;
-      expectedScores = this._getExpectedScores(this._ratingA, this._ratingB);
-      this._expectedA = expectedScores.a;
-      this._expectedB = expectedScores.b;
-      newRatings = this._getNewRatings(this._ratingA, this._ratingB, this._expectedA, this._expectedB, this._scoreA, this._scoreB);
-      this._newRatingA = newRatings.a;
-      this._newRatingB = newRatings.b;
-    }
+var RangeMin = 50;
+var RangeMax = 100;
 
-    EloRating.prototype.setNewSetings = function(ratingA, ratingB, scoreA, scoreB) {
-      var expectedScores, newRatings;
-      this._ratingA = ratingA;
-      this._ratingB = ratingB;
-      this._scoreA = scoreA;
-      this._scoreB = scoreB;
-      expectedScores = this._getExpectedScores(this._ratingA, this._ratingB);
-      this._expectedA = expectedScores.a;
-      this._expectedB = expectedScores.b;
-      newRatings = this._getNewRatings(this._ratingA, this._ratingB, this._expectedA, this._expectedB, this._scoreA, this._scoreB);
-      this._newRatingA = newRatings.a;
-      return this._newRatingB = newRatings.b;
-    };
+var KValue1 = 40;
+var KValue2 = 20;
+var KValue3 = 10;
 
-    EloRating.prototype.getNewRatings = function() {
-      var ratings;
-      return ratings = {
-        a: Math.round(this._newRatingA),
-        b: Math.round(this._newRatingB)
-      };
-    };
+EloRating.EloRating = function (ratingA, ratingB, scoreA, scoreB){
 
-    EloRating.prototype._getExpectedScores = function(ratingA, ratingB) {
-      var expected, expectedScoreA, expectedScoreB;
-      expectedScoreA = 1 / (1 + Math.pow(10, (ratingB - ratingA) / 400));
-      expectedScoreB = 1 / (1 + Math.pow(10, (ratingA - ratingB) / 400));
-      return expected = {
-        a: expectedScoreA,
-        b: expectedScoreB
-      };
-    };
+  var expectedScores, newRatings;
+  this.KFACTOR = 16;
+  this._ratingA = ratingA;
+  this._ratingB = ratingB;
+  this._scoreA = scoreA;
+  this._scoreB = scoreB;
+  expectedScores = this._getExpectedScores(this._ratingA, this._ratingB);
+  this._expectedA = expectedScores.a;
+  this._expectedB = expectedScores.b;
+  newRatings = this._getNewRatings(this._ratingA, this._ratingB, this._expectedA, this._expectedB, this._scoreA, this._scoreB);
+  this._newRatingA = newRatings.a;
+  this._newRatingB = newRatings.b;
+}
 
-    EloRating.prototype._getNewRatings = function(ratingA, ratingB, expectedA, expectedB, scoreA, scoreB) {
-      var newRatingA, newRatingB, ratings;
-      newRatingA = ratingA + (this.KFACTOR * (scoreA - expectedA));
-      newRatingB = ratingB + (this.KFACTOR * (scoreB - expectedB));
-      return ratings = {
-        a: newRatingA,
-        b: newRatingB
-      };
-    };
+EloRating.setNewSetings = function(ratingA, ratingB, scoreA, scoreB) {
+  var expectedScores, newRatings;
+  this._ratingA = ratingA;
+  this._ratingB = ratingB;
+  this._scoreA = scoreA;
+  this._scoreB = scoreB;
+  expectedScores = this._getExpectedScores(this._ratingA, this._ratingB);
+  this._expectedA = expectedScores.a;
+  this._expectedB = expectedScores.b;
+  newRatings = this._getNewRatings(this._ratingA, this._ratingB, this._expectedA, this._expectedB, this._scoreA, this._scoreB);
+  this._newRatingA = newRatings.a;
+  return this._newRatingB = newRatings.b;
+};
 
-    return EloRating;
+EloRating.getNewRatings = function() {
+  var ratings;
+  return ratings = {
+    a: Math.round(this._newRatingA),
+    b: Math.round(this._newRatingB)
+  };
+};
 
-  })();
+EloRating._getExpectedScores = function(ratingA, ratingB) {
+  var expected, expectedScoreA, expectedScoreB;
+  expectedScoreA = 1 / (1 + Math.pow(10, (ratingB - ratingA) / 400));
+  expectedScoreB = 1 / (1 + Math.pow(10, (ratingA - ratingB) / 400));
+  return expected = {
+    a: expectedScoreA,
+    b: expectedScoreB
+  };
+};
 
-  elo = new EloRating();
+EloRating._getNewRatings = function(ratingA, ratingB, expectedA, expectedB, scoreA, scoreB) {
+  var newRatingA, newRatingB, ratings;
+  newRatingA = ratingA + (this.KFACTOR * (scoreA - expectedA));
+  newRatingB = ratingB + (this.KFACTOR * (scoreB - expectedB));
+  return ratings = {
+    a: newRatingA,
+    b: newRatingB
+  };
+};
